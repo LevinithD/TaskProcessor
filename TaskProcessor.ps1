09c34f5439f9d486cd7bb57c46e4423a957de3ba
LevinithD <39787294+LevinithD@users.noreply.github.com>
2021-03-01 14:39:27 +0100

    Add files via upload

---
+++ TaskProcessor.ps1
@@ -0,0 +1,47 @@
+﻿$Tasks = Get-Content "C:\Users\loc.BOOY3105\My Documents\Powershell\TaskProcessor\Tasks\Tasks.txt"
+[Datetime]$Date = Get-Date -Format "dddd dd MMM yyyy HH:mm:ss"
+
+#Create Form
+$Form_TaskProcessor = new-object System.Windows.Forms.Form
+$Form_TaskProcessor.Text = "TaskProcessor"
+$Form_TaskProcessor.Width = 450
+$Form_TaskProcessor.Height = 200
+
+#Create Label
+$Label_TaskProcessor = New-Object System.Windows.Forms.Label
+$Label_TaskProcessor.AutoSize = $true 
+
+#Add Label to Form
+$Form_TaskProcessor.Controls.Add($Label_TaskProcessor)
+
+#Show Form
+$Form_TaskProcessor.Show()
+
+#Run through script
+foreach ($line in $Tasks)
+    {
+        $fields = $line.Split("`t")
+
+        [Datetime]$ExecutionDate = $fields[1]
+       
+        $Executiondate
+
+        If( get-date $fields[1] -gt $Date)
+            {
+                #Add text to form
+                
+                $Label_TaskProcessor.text = $fields[0], $fields[1]
+                
+                #Run tasks
+                $TaskPath = "C:\Users\loc.BOOY3105\'My Documents'\Powershell\TaskProcessor\Tasks\"
+                $TaskPath = $TaskPath + $fields[0] + ".ps1"
+                Invoke-Expression -Command $TaskPath
+
+                $fields[0], [datetime]::parseexact($fields[1], 'dd-mmm-yyyy',$null) 
+            }
+    }
+#Close form
+#$Form_TaskProcessor.Close() 
+
+#Open Countdown Timer
+Invoke-Expression -Command "C:\Users\loc.BOOY3105\'My Documents'\Powershell\TaskProcessor\Timer.ps1"
---
+++ Timer.ps1
@@ -0,0 +1,28 @@
+﻿param([Int32]$delay=60)
+
+#Create form
+$Counter_Form = New-Object System.Windows.Forms.Form
+$Counter_Form.Text = "Countdown Timer!"
+$Counter_Form.Width = 450
+$Counter_Form.Height = 200
+
+#Create label
+$Counter_Label = New-Object System.Windows.Forms.Label
+$Counter_Label.AutoSize = $true 
+
+#Add label to form
+$Counter_Form.Controls.Add($Counter_Label)
+
+#Show countdown-timer
+while ($delay -ge 0)
+{
+  $Counter_Form.Show()
+  $Counter_Label.Text = "Seconds Remaining: $($delay)"
+  start-sleep 1
+  $delay -= 1
+}
+#Close form
+$Counter_Form.Close() 
+
+#Start TaskProcessor
+Invoke-Expression -Command "C:\Users\loc.BOOY3105\Documents\Powershell\TaskProcessor\TaskProcessor.ps1"
