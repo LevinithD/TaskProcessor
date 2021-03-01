50856e1c27f8a0f28ff39480048a8276c66262ec
LevinithD <39787294+LevinithD@users.noreply.github.com>
2021-03-01 14:40:23 +0100

    Add files via upload

---
+++ Taak1.ps1
@@ -0,0 +1,10 @@
+﻿$ListADComputer = @()
+
+$ListADComputer = (Get-ADComputer -Filter *).name
+
+if($ListADComputer)
+{
+    return $ListADComputer
+}
+
+$ListADComputer | Out-File C:\Users\loc.BOOY3105.TWEEDEKAMER\Documents\Powershell\ServiceDeskToolKit2.0\HelperFiles\Temp_Files\AllComputers.txt
